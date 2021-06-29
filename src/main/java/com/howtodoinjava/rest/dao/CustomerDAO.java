60e0ea94201488ac71784d7fdc3ec85852e26ec2
Reuel B. Albuquerque Jr <reuelja@gmail.com>
2021-06-28 21:52:21 -0300

    customerdao

--- src/main/java/com/howtodoinjava/rest/dao/CustomerDAO.java
+++ src/main/java/com/howtodoinjava/rest/dao/CustomerDAO.java
@@ -7,27 +7,25 @@ import com.howtodoinjava.rest.model.Customers;
 import com.howtodoinjava.rest.model.Telefone;
 
 @Repository
-public class CustomerDAO 
-{
+public class CustomerDAO {
     private static Customers list = new Customers();
-    
-    public static Telefone t1 = new Telefone(1,"21","976665879");
-    public static Telefone t2 = new Telefone(2,"21","978658897");
-    public static Telefone t3 = new Telefone(3,"23","987451614");
+
+    public static Telefone t1 = new Telefone(1, "21", "976665879");
+    public static Telefone t2 = new Telefone(2, "21", "978658897");
+    public static Telefone t3 = new Telefone(3, "23", "987451614");
     public static Customer c = new Customer(1, "Ricardo", "null", "65487941217", "r.zontah@gmail.com", t1);
-    static 
-    
+    static
+
     {
         list.getCustomerList().add(c);
         list.getCustomerList().add(new Customer(2, "Igor", "null", "71257688714", "iof7@sony.com", t2));
         list.getCustomerList().add(new Customer(3, "Michael", "null", "53691236823", "michael@andrettiracing.com", t3));
     }
-    
-    public Customers getAllCustomers() 
-    {
+
+    public Customers getAllCustomers() {
         return list;
     }
-    
+
     public void addCustomer(Customer customer) {
         list.getCustomerList().add(customer);
     }
